96b79023feabaf03fcb36cbf8b99700e6cc83928
yohannes1097 <yohannes10.97@gmail.com>
2020-09-26 12:03:50 +0700

    25/09 Search bar, Footer, Properti Location Section

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,72 @@
 @section('content')
     @yield('app_carousel', View::make('layouts.app_carousel'))
     <br>
+    <div class="container bg-default rounded">
+    <br>
+        <form action="#" method="get">
+            <div class="d-flex">
+                <div class="form-group mr-3" style="min-width:10rem;">
+                    <select name="" id="" class="form-control">
+                        <option value="#">Beli / Sewa</option>
+                        <option value="#">Beli</option>
+                        <option value="#">Sewa</option>
+                    </select>
+                </div>
+                <div class="form-group input-group input-group-merge">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text"><i class="fas fa-search"></i></span>
+                    </div>
+                    <input class="form-control" placeholder="Your name" type="text">
+                </div>
+                <div class="form-group ml-3">
+                    <button type="button" class="btn btn-outline-secondary text-white btn-block">Search</button>
+                </div>
+            </div>
+            <div class="d-flex">
+                <div class="form-group mr-3">
+                    <select name="" id="" class="form-control">
+                        <option value="#">Baru / Bekas</option>
+                        <option value="#">Baru</option>
+                        <option value="#">Bekas</option>
+                        <option value="#">Semua</option>
+                    </select>
+                </div>
+                <div class="form-group mr-3">
+                    <select name="" id="" class="form-control">
+                        <option value="#">Jenis Hunian</option>
+                        <option value="#">Apartemen</option>
+                        <option value="#">Rumah</option>
+                        <option value="#">Tanah</option>
+                        <option value="#">Ruko</option>
+                        <option value="#">Vila</option>
+                    </select>
+                </div>
+                <div class="form-group mr-3">
+                    <select name="" id="" class="form-control">
+                        <option value="#">Harga Minimal</option>
+                        <option value="#">50 Juta</option>
+                        <option value="#">100 Juta</option>
+                        <option value="#">200 Juta</option>
+                        <option value="#">500 Juta</option>
+                        <option value="#">1 Milyar</option>
+                        <option value="#">2 Milyar</option>
+                    </select>
+                </div>
+                <div class="form-group mr-3">
+                    <select name="" id="" class="form-control">
+                        <option value="#">Harga Maksimal</option>
+                        <option value="#">50 Juta</option>
+                        <option value="#">100 Juta</option>
+                        <option value="#">200 Juta</option>
+                        <option value="#">500 Juta</option>
+                        <option value="#">1 Milyar</option>
+                        <option value="#">2 Milyar+</option>
+                    </select>
+                </div>
+            </div>
+        </form>
+    </div>
+    <br>
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-sm-6 col-md-4">
@@ -180,4 +246,78 @@
             </div>
         </div>
     </div>
+
+    <div class="container-fluid">
+        <div class="row justify-content-around">
+            <div class="col-md 3">
+                <div><a href="#">Properti di DKI Jakarta</a></div>
+                <div><a href="#">Properti di Bali</a></div>
+                <div><a href="#">Properti di Bandung</a></div>
+                <div><a href="#">Properti di Banten</a></div>
+                <div><a href="#">Properti di Depok</a></div>
+            </div>
+            <div class="col-md 3">
+                <div><a href="#">Properti di Tanggerang</a></div>
+                <div><a href="#">Properti di Daerah Istimewa Yogyakarta</a></div>
+                <div><a href="#">Properti di Semarang</a></div>
+                <div><a href="#">Properti di Surabaya</a></div>
+                <div><a href="#">Properti di Bekasi</a></div>
+            </div>
+            <div class="col-md 3">
+                <div><a href="#">Properti di DKI Jakarta</a></div>
+                <div><a href="#">Properti di Bali</a></div>
+                <div><a href="#">Properti di Bandung</a></div>
+                <div><a href="#">Properti di Banten</a></div>
+                <div><a href="#">Properti di Depok</a></div>
+            </div>
+            <div class="col-md 3">
+                <div><a href="#">Properti di Tanggerang</a></div>
+                <div><a href="#">Properti di Daerah Istimewa Yogyakarta</a></div>
+                <div><a href="#">Properti di Semarang</a></div>
+                <div><a href="#">Properti di Surabaya</a></div>
+                <div><a href="#">Properti di Bekasi</a></div>
+            </div>
+        </div>
+    </div>
+
+    <br>
+
+    <footer class="page-footer font-small blue pt-4 bg-dark text-secondary">
+        <div class="container-fluid">
+
+            <div class="d-flex justify-content-around text-center">
+                <div><a href="#"><i class="fab fa-facebook fa-2x"></i></a></div>
+                <div><a href="#"><i class="fab fa-whatsapp fa-2x"></i></a></div>
+                <div><a href="#"><i class="fab fa-twitter fa-2x"></i></a></div>
+                <div><a href="#"><i class="fab fa-instagram fa-2x"></i></a></div>
+                <div><a href="#"><i class="fab fa-youtube fa-2x"></i></a></div>
+            </div>
+
+            <br>
+
+            <div class="row justify-content-around text-center">
+                <div class="col-sm-6 col-md-2"><a href="#" class="px-auto">Tentang Kami</a></div>
+                <div class="col-sm-6 col-md-2"><a href="#">Kontak Kami</a></div>
+                <div class="col-sm-6 col-md-2"><a href="#">Iklankan Properti Anda</a></div>
+                <div class="col-sm-6 col-md-2"><a href="#">Kebijakan Privasi</a></div>
+                <div class="col-sm-6 col-md-2"><a href="#">Syarat Penggunaan</a></div>
+                <div class="col-sm-6 col-md-2"><a href="#">FAQ</a></div>
+            </div>
+
+            <br>
+
+            <div class="row">
+                <div class="col-md-3">
+                    <h3 class="text-secondary">Hubungi Kami :</h3>
+                    <p><small>Lebak Permai Utara 1, Kenjeran, Surabaya</small></p>
+                    <p><small>Call : 0812 - 8662 - 8888</small></p>
+                    <p><small>admin@lokasihunian.com</small></p>
+                </div>
+            </div>
+
+            <div class="footer-copyright text-center py-3">© 2020 
+                <a href="{{ url('') }}"> PanenWeb Group</a>
+            </div>
+        </div>
+    </footer>
 @endsection
