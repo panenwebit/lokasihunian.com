13cf28a04c9894f540f538cd317c60e71bc147b7
yohannes1097 <yohannes10.97@gmail.com>
2020-10-03 12:01:47 +0700

    03/10 Testing google maps api

--- resources/views/test/map.blade.php
+++ resources/views/test/map.blade.php
@@ -15,10 +15,31 @@
 <body>
     <h3>My Google Maps Demo</h3>
     <!--The div element for the map -->
+    <div id="map"></div>
     <div style="overflow:hidden;resize:none;max-width:100%;width:80vw;height:80vh;">
         <div style="height:100%; width:100%;max-width:100%;">
             <iframe style="height:100%;width:100%;border:0;" frameborder="0" src="https://www.google.com/maps/embed/v1/place?q=-7.2690805,112.8040827&key=AIzaSyDY7KXDCH8pYaoOycJTAkhER2f-QzCRgI8"></iframe>
         </div>
     </div>
+    <script>
+        // Initialize and add the map
+        function initMap() {
+            // The location of Uluru
+            var uluru = {lat: -25.344, lng: 131.036};
+            // The map, centered at Uluru
+            var map = new google.maps.Map(
+                document.getElementById('map'), {zoom: 4, center: uluru});
+                // The marker, positioned at Uluru
+                var marker = new google.maps.Marker({position: uluru, map: map});
+            }
+    </script>
+    <!--Load the API from the specified URL
+    * The async attribute allows the browser to render the page while the API loads
+    * The key parameter will contain your own API key (which is not needed for this tutorial)
+    * The callback parameter executes the initMap() function
+    -->
+    <script defer
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDGpzOgrb6WitlfZP7DNYlN3JLZCkNnIuY&callback=initMap">
+    </script>
 </body>
 </html>
